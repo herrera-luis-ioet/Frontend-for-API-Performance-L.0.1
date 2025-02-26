512a483be17e05de18f9012e6677e258a268049e
root <root@ip-172-31-35-95.ec2.internal>
2025-02-26 17:45:49 +0000

    fix(setup): resolve build issues and clean up duplicate code in SPA component

--- src/App.js
+++ src/App.js
@@ -14,20 +14,4 @@ function App() {
   );
 }
 
-export default App;import React from 'react';
-
-// PUBLIC_INTERFACE
-function App() {
-  return (
-    <div className="app">
-      <header className="app-header">
-        <h1>API Performance Analysis</h1>
-      </header>
-      <main className="app-main">
-        {/* Main content will be added here */}
-      </main>
-    </div>
-  );
-}
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -10,16 +10,4 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './styles/index.css';
-import App from './App';
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
 );
